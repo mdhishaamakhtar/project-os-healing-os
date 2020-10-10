6848d8a7df666d603b8aba6870c44c158e345d63
Md Hishaam Akhtar <58990970+mdhishaamakhtar@users.noreply.github.com>
2020-10-10 14:58:57 +0530

    Move to app (#15)
    Co-authored-by: Yash Kumar Verma <yk.verma2000@gmail.com>

--- source/app.cpp
+++ source/app.cpp
@@ -1,14 +1,15 @@
 #include<iostream>
 #include"./../includes/rainbow/rainbow.h"
+#include"./../includes/watcher/watcher.h"
 #include"./../includes/logger/logger.cpp"
+#include <sstream>
+#include<string>
 #include"./../includes/watcher/watcher.h"
 #include"./../includes/log_processor/log_processor.h"
 
 using namespace std;
-
 int main(){
-    cout << rainbow::underline(rainbow::bold(rainbow::red("SELF HEALING OS"))) << endl << endl; 
-    
+    cout << rainbow::underline(rainbow::bold(rainbow::red("SELF HEALING OS"))) << endl << endl;
     cout << rainbow::bold("Logger Usage") << endl << endl;
 
     LoggerModule logger;
@@ -21,6 +22,43 @@ int main(){
     logger.save("system", "vim task.md");
     logger.save("system", "rm *.txt");
     logger.save("internal", ":checkpoint create");
+    logger.save("internal", ":checkpoint delete");
+  
+  
+    Watcher w;
+    LoggerClass l;
+    while (true) {
+        std::string command = w.enter_command();
+        l.logInfo(command, "command");
+        if (command == "exit") {
+            break;
+        }
+        if (command[0] != ':') {
+            std::cout << "Given command is a system command" << std::endl;
+            char cmd[command.size() + 1];
+            strcpy(cmd, command.c_str());
+            system(cmd);
+            continue;
+        }
+        std::string word = "";
+        std::vector<string> s;
+        std::stringstream iss(command);
+        while (iss >> word) {
+            s.push_back(word);
+        }
+        if (s.size() <= 1) {
+            std::cout << "Please enter the correct command again" << std::endl;
+            continue;
+        }
+        string command_to_pass = s.at(0) + " " + s.at(1);
+        if (!w.command_checker(command_to_pass)) {
+            std::cout << "Please enter the correct command again" << std::endl;
+            continue;
+        }
+        std::cout << "Command executed" << std::endl;
+        return 0;
+    }
+
 
 
     cout << endl << rainbow::bold(rainbow::underline("Reading and Processing Logs")) << endl;
@@ -31,6 +69,7 @@ int main(){
     for(auto entry : logProcessor.Fetch(1602321815)){
         cout << entry.timestamp << " : " << entry.command << endl;
     }
+
 
     return 0;
 }
