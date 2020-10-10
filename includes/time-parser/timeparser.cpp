2aee5cb87f7d6cb9967ba605f06a66a8693725e1
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2020-10-10 16:06:32 +0530

    Add comments

--- includes/time-parser/timeparser.cpp
+++ includes/time-parser/timeparser.cpp
@@ -5,11 +5,27 @@
 using namespace std;
 using namespace std::chrono;
 using namespace date;
+
+/**
+ * @class TimeParser
+ * To parse the time taken in linux format and convert it to a readable one
+ */
 class TimeParser {
     public:
+        /**
+         * @param time_t s
+         * Declaration of parseTime function
+         */
         date::year_month_day parseTime(time_t s);
 };
 
+/**
+ * @param time_t time
+ * Takes in unix time as a parameter
+ * Returns the date in a readable format
+ * Example input: 1602325928
+ * Example output: 2020-10-10
+ */
 date::year_month_day TimeParser::parseTime(time_t time){
     auto sysTime = std::chrono::system_clock::from_time_t(time);
     auto date = year_month_day(floor<days>(sysTime));
