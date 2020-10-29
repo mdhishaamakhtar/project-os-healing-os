fca1504215d4ca6cdcad56bffe79a8e67ba54b4a
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2020-10-29 19:50:13 +0530

    Removed unnecessary comments

--- includes/watcher/watcher.cpp
+++ includes/watcher/watcher.cpp
@@ -9,40 +9,6 @@
 
 using namespace std;
 
-// class Watcher {
-
-// public:
-//     /**
-//      * Vector which stores all commands available for the User
-//      * More to be added when core utility is implemented
-//      */
-//     vector<string> available_commands = {
-//         ":checkpoint create",
-//         ":checkpoint revert",
-//         ":branch new",
-//         ":branch switch",
-//         ":branch delete",
-//         ":branch accept",
-//         ":cloud attach",
-//         ":cloud pull",
-//         ":cloud push",
-//         ":logs show",
-//         ":logs clean"
-//     };
-
-//     /**Function to enter a command */
-//     string listenForCommand(string context);
-
-//     /**
-//      * Function to call the utility function to
-//      * be implemented when the functions are ready
-//      */
-//     void taskDispatcher(vector<string> command);
-
-//     /** Functon to check if command is allowed or not */
-//     bool ifValidInternalCommand(string command);
-// };
-
 /** Implementation of enter_command */
 string Watcher::listenForCommand(string context) {
     string command = "";
